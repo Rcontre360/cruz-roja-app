55a3e10e09539d318a6387291d78bcca6f63e843
Rcontre360 <rcontreraspimentel@gmail.com>
2025-02-10 09:29:46 -0400

    feat: latest server changes

--- server/src/common/env.ts
+++ server/src/common/env.ts
@@ -1,21 +1,20 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config({path: `${__dirname}/../../.env.${process.env.NODE_ENV}`});
 
-import {cleanEnv, host, num, port, str, testOnly} from 'envalid';
+import {cleanEnv, host, num, port, str, testOnly} from "envalid";
 
 export const env = cleanEnv(process.env, {
-  NODE_ENV: str({devDefault: testOnly('test'), choices: ['dev', 'prod', 'test']}),
-  HOST: host({devDefault: testOnly('localhost')}),
+  NODE_ENV: str({devDefault: testOnly("test"), choices: ["dev", "prod", "test"]}),
+  HOST: host({devDefault: testOnly("localhost")}),
 
   SERVER_PORT: port({devDefault: testOnly(3000)}),
-  CORS_ORIGIN: str({devDefault: testOnly('http://localhost:3000')}),
+  CORS_ORIGIN: str({devDefault: testOnly("*")}),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({devDefault: 1000}),
   COMMON_RATE_LIMIT_WINDOW_MS: num({devDefault: 1000}),
-  PINO_LOG_LEVEL: str({devDefault: 'debug', default: 'info'}),
+  PINO_LOG_LEVEL: str({devDefault: "debug", default: "info"}),
 
-  POSTGRES_ENDPOINT: str({devDefault: ''}),
-  JWT_SECRET: str({devDefault: ''}),
-  JWT_EXPIRATION: str({devDefault: '7d'}),
+  POSTGRES_ENDPOINT: str({devDefault: ""}),
+  JWT_SECRET: str({devDefault: ""}),
+  JWT_EXPIRATION: str({devDefault: "7d"}),
   PASSWORD_SALT: num({default: 10}),
 });
-
