db08b7efbdf24211ab7a02b1fd4593cabd836e6b
Rcontre360 <rcontreraspimentel@gmail.com>
2025-04-06 20:45:54 -0400

    feat: programs table

--- app/src/pages/programs/edit/[id].tsx
+++ app/src/pages/programs/edit/[id].tsx
@@ -1,17 +1,17 @@
 import {Field, Form, Formik} from 'formik'
 import Head from 'next/head'
 import {ReactElement} from 'react'
-import NotificationBar from '../../components/NotificationBar'
-import Button from '../../components/Button'
-import Buttons from '../../components/Buttons'
-import CardBox from '../../components/CardBox'
-import FormField from '../../components/Form/Field'
-import LayoutAuthenticated from '../../layouts/Authenticated'
-import SectionMain from '../../components/Section/Main'
-import SectionTitle from '../../components/Section/Title'
-import {getPageTitle} from '../../config'
-import {useAppDispatch, useAppSelector} from '../../stores/hooks'
-import {onEditProgram} from '../../stores/actions/programs'
+import NotificationBar from '../../../components/NotificationBar'
+import Button from '../../../components/Button'
+import Buttons from '../../../components/Buttons'
+import CardBox from '../../../components/CardBox'
+import FormField from '../../../components/Form/Field'
+import LayoutAuthenticated from '../../../layouts/Authenticated'
+import SectionMain from '../../../components/Section/Main'
+import SectionTitle from '../../../components/Section/Title'
+import {getPageTitle} from '../../../config'
+import {useAppDispatch, useAppSelector} from '../../../stores/hooks'
+import {onEditProgram} from '../../../stores/actions/programs'
 import {useRouter} from 'next/router'
 
 const EditProgramPage = () => {
@@ -21,6 +21,7 @@ const EditProgramPage = () => {
 
   const handleSubmit = async (program: {description: string}) => {
     await dispatch(onEditProgram({programId: router.query.id as string, fields: program}))
+    router.push('/programs')
   }
 
   return (
@@ -42,7 +43,7 @@ const EditProgramPage = () => {
           >
             <Form>
               <FormField label="Descripcion">
-                <Field type="text" name="descrciption" placeholder="Descripcion" />
+                <Field type="text" name="description" placeholder="Descripcion" />
               </FormField>
               <Buttons>
                 <Button type="submit" color="info" label="Submit" />
